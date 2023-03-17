9d21ddcdc712182d5e7365de455f1bcf23183923
HaroldF415 <harold.f415@gmail.com>
2023-03-17 11:57:53 -0400

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import ImageCard from "./components/ImageCard/ImageCard";
+import { useState, useEffect } from "react";
+import fetchImages from "./utils/fetch.js";
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchImages().then((res) => {
+      setImages(res);
+    });
+  }, []);
+
   return (
     <div className="App">
-<h1>here and there</h1>
+      <h1>Unsplash API v2 by Riya Alexander, Harold Fachin, Diandre Miller, Keeanu Rodriguez</h1>
+      {images.map((image) => {
+        return (
+          <div>
+            <ImageCard img={image} />
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,9 +1,10 @@
-const axios = require("axios");
-const access_key = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+import axios from "axios";
+const access_key = process.env.REACT_APP_UNSPLASH_ACCESS;
 const secret_key = process.env.REACT_APP_UNSPLASH_SECRET_KEY;
 
 const fetchImages = async () => {
   const { data } = await axios.get(`https://api.unsplash.com/photos/random?client_id=${access_key}&count=5`);
+  console.log(data);
   return data;
 };
 
